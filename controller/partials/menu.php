49a74d74fdf7576c7b0046a95079ff624481405a
IMac <imac@IMacs-iMac.Dlink>
2022-12-21 23:31:42 +0600

    initial

---
+++ controller/partials/menu.php
@@ -0,0 +1,30 @@
+<?php 
+
+    include('../model/constants.php'); 
+    include('login-check.php');
+
+?>
+
+
+<html>
+    <head>
+        <title>Bakery Order Website - Home Page</title>
+
+        <link rel="stylesheet" href="../view/admin.css">
+    </head>
+    
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="manage-category.php">Category</a></li>
+                    <li><a href="manage-food.php">Bakery-Items</a></li>
+                    <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Menu Section Ends -->
